427af9f6db06478b2fdbb540d2ba34c3abce10bf
keerthika257 <keerthivaheesan@gmail.com>
2020-06-12 23:18:49 +0530

    10 binaries uploaded

--- src/app/directives/menu.dir.ts
+++ src/app/directives/menu.dir.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector : 'menu',
   template : `
+
+
+
+
+
+  
   <nav class="navbar navbar-expand-lg navbar-light bg-light d-print-none">
   <a class="navbar-brand" href="#" routerLink = '/products'><img src="https://cdn.pixabay.com/photo/2015/11/06/15/13/internet-1028794_960_720.jpg" class="logo mr-2"/>Online Shopping</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
